269d0410807993aff08e2f5ce6d781d80029d89a
manish-mdr <maanish123mdr@gmail.com>
2026-03-14 15:22:44 +0900

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //  treat all JS code as newer version
+
+// alert("Manish");     // we are using node.js, not browser
+
+let name = "Manish";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null); // output is object
+console.log(typeof undefined); // output is undefined
