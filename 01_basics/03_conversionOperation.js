5a890a18bb045dc1c30b10da4bca277e06bae030
manish-mdr <maanish123mdr@gmail.com>
2026-03-14 17:34:52 +0900

    Data Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// convert score from string to number
+// let valueInNum = Number(score);
+
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
