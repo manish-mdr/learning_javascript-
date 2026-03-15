71167ce918c60807d966a92c929c265dfbf377a9
manish-mdr <maanish123mdr@gmail.com>
2026-03-15 16:53:21 +0900

    learning of String and function

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const name = "Manish";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Manish-Mdr");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt("5"));
+console.log(gameName.indexOf("h"));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
