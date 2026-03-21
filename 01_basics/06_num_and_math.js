b1851395bbd62681377138f04263f6a84caa7281
manish-mdr <maanish123mdr@gmail.com>
2026-03-21 18:50:30 +0900

    math and num

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,32 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 1123.8966;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// ============== Maths =========
+// console.log(Math);
+// console.log(Math.abs(-4)); //  output : 4
+// console.log(Math.round(5.64)); // output : 6
+// console.log(Math.ceil(5.64)); // output : 6
+// console.log(Math.floor(5.64)); // output : 5
+// console.log(Math.min(4, 3, 5, 5, 7)); //output : 3
+// console.log(Math.max(4, 3, 5, 5, 7)); // output : 7
+
+// console.log(Math.random()); //always return between 0 to 1
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min +1)) + min);
